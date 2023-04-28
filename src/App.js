0f0fa3bc92e4350e8ccfd477924bfb6a047457a8
Da Antonio <daantonioc3@gmail.com>
2023-04-28 01:10:14 +0000

    advanced_props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Phrase from './components/phrase';
